de55f8bf731270745c2eed4f6eecf3747ca463a3
Ferhat Karasu <74055034+ferhatkarasu@users.noreply.github.com>
2023-10-22 15:30:44 +0300

    Create index.php

---
+++ 001-Find The Largest Number/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML>  
+<html>
+<head>
+
+<h1>Find The Largest Number</h1>
+
+</head>
+<body>  
+<form action="calculate.php" method="post">
+
+    <input type="number" name="num1" placeholder="Enter the first number : ">
+    <br>
+    <br>
+    <input type="number" name="num2" placeholder="Enter the second number : ">
+    <br>
+    <br>
+    <input type="number" name="num3" placeholder="Enter the third number : ">
+    <br>
+    <br>
+    <button type="submit">Calculate</button>
+
+</form>
+
+</body>
+</html>
